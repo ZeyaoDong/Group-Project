0611c420cb159d31c015b375022efea76f76744d
Tom Wise <tomkennedywise@gmail.com>
2023-10-18 17:12:14 +0100

    I have added a class called Dosing_Protocol to the protocol.py file, will add more comments shortly. Will definitely need some edits.

--- pkmodel/protocol.py
+++ pkmodel/protocol.py
@@ -2,16 +2,54 @@
 # Protocol class
 #
 
-class Protocol:
-    """A Pharmokinetic (PK) protocol
+class Dosing_Protocol:
+    def __init__(self, dosing_method, dose_amount=0, interval=0, duration=0):
+        self.dosing_method = dosing_method
+        self.dose_amount = dose_amount
+        self.interval = interval
+        self.duration = duration
+        if dose_amount < 0:
+            raise ValueError("Dose_amount must be positive")
+        if interval < 0:
+            raise ValueError("Time interval for drug adminitration must be positive")
+        if interval != 0 and dosing_method == 1:
+            raise TypeError("For dosing method 1, no interval argument is needed")
+        if duration < 0:
+            raise ValueError("Time duration for dosing method 2 has to be positive")
+        if duration != 0 and dosing_method == 1:
+            raise TypeError("For dosing method 1, no duration argument is needed")
+            
 
-    Parameters
-    ----------
+    def get_dose(self, t):
+        if self.dosing_method == 1:
+            # Single, time-independent administration
+            if t == 0:
+                return self.dose_amount
+            else:
+                return 0
 
-    value: numeric, optional
-        an example paramter
+        elif self.dosing_method == 2:
+            # Repeated administration at equal intervals
+            if t < self.duration:
+                if t % self.interval == 0:
+                    return self.dose_amount
+                else:
+                    return 0
+            else:
+                return 0
 
-    """
-    def __init__(self, value=43):
-        self.value = value
+        else:
+            raise ValueError("Invalid dosing method")
+
+    def get_dose_function(self):
+        if self.dosing_method == 1:
+            # Single, time-independent administration
+            return lambda t: self.get_dose(t)
+
+        elif self.dosing_method == 2:
+            # Repeated administration at equal intervals
+            return lambda t: self.get_dose(t)
+
+        else:
+            raise ValueError("Invalid dosing method")
 
