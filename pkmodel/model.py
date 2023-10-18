2b897e4444b9e9e6b78a5ecf87f6ef9e5c51e7d5
ZeyaoDong <147256097+ZeyaoDong@users.noreply.github.com>
2023-10-18 10:29:35 +0100

    Update model.py
    still separate for different types of dosing

--- pkmodel/model.py
+++ pkmodel/model.py
@@ -1,17 +1,48 @@
-#
-# Model class
-#
+import numpy as np
+from scipy.integrate import solve_ivp
 
-class Model:
-    """A Pharmokinetic (PK) model
 
-    Parameters
-    ----------
+class BolusModel:
+    def __init__(self, number_of_p_compartments,initial_values):
+        self.p_compartmemts = number_of_p_compartments
+        self.initial_values = initial_values
 
-    value: numeric, optional
-        an example paramter
+    def __str__(self):
+        return 'This is a PK model for intravenous bolus dosing with '+ f'{self.p_compartmemts} peripheral compartment(s)'
 
-    """
-    def __init__(self, value=42):
-        self.value = value
+    def ODE(self, t, q, transition_rate, elimination_rate, volume_c, volume_q):
+    #def ODE(self, t, transition_rate, elimination_rate, volume_c, volume_q):
+        qc, *qp = q 
+        dosing_protocol = 0 # get dosing protocol from another .py file
+        density_difference = [qc/volume_c - p/volume_q for p in qp]
+        flux = [k*diff for k, diff in zip(transition_rate, density_difference)]
 
+        dq_dt = [0.0]*len(q)
+
+        # calculate for the cental compartment
+        dq_dt[0] = dosing_protocol - elimination_rate*qc - np.sum(flux)
+
+        # calculate for the peripheral compartments
+        
+        for i in range(len(transition_rate)):
+            dq_dt[i+1] = flux[i]
+        
+        
+        return dq_dt
+    
+    # use another separat.py file to solve the ODE?
+    def solve_ODE(self, initial_values, transition_rate, elimination_rate, volume_c, volume_q, t_span, t_eval):
+        solution = solve_ivp(
+            fun = lambda t, q: self.ODE(t,q, transition_rate, elimination_rate, volume_c, volume_q), 
+            t_span = t_span,
+            y0 = initial_values, 
+            t_eval = t_eval)
+        
+        return solution.y
+
+class SubcutaneousModel:
+    def __init__(self, number_of_p_compartments):
+        self.p_compartmemts = number_of_p_compartments
+
+    def __str__(self, number_of_compartments):
+        return 'This is a PK model for subcutaneous dosing with '+ f'{self.p_compartmemts} peripheral compartment(s)'
