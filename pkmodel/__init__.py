a4b8b96b4c7a9d04686740c86e49b0bd8b5ee2f9
ZeyaoDong <147256097+ZeyaoDong@users.noreply.github.com>
2023-10-20 09:49:54 +0100

    Update __init__.py

--- pkmodel/__init__.py
+++ pkmodel/__init__.py
@@ -8,6 +8,6 @@ of Parmokinetic (PK) models
 from .version_info import VERSION_INT, VERSION  # noqa
 
 # Import main classes
-from .model import Model    # noqa
-from .protocol import Protocol    # noqa
-from .solution import Solution     # noqa
+from .model import PKModel  
+from .protocol import Dosing_Protocol    
+from .visualiser import visualize     
