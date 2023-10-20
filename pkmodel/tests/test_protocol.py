6f7a4b7c198be614708a61620c35b4413b9d1906
ZeyaoDong <147256097+ZeyaoDong@users.noreply.github.com>
2023-10-20 16:21:06 +0100

    Update test_protocol.py

--- pkmodel/tests/test_protocol.py
+++ pkmodel/tests/test_protocol.py
@@ -1,6 +1,5 @@
 
 import unittest
-import pkmodel as pk
 from pkmodel.protocol import Dosing_Protocol
 
 class Dosing_Protocol_test(unittest.TestCase):
@@ -12,7 +11,7 @@ class Dosing_Protocol_test(unittest.TestCase):
         Tests Protocol creation
     """
         #Test Dosing_protocol with specified parameters as expected values
-        protocol = pk.Dosing_Protocol(2, 3, 4, 5)
+        protocol = Dosing_Protocol(2, 3, 4, 5)
         self.assertEqual(protocol.dosing_method, 2)
         self.assertEqual(protocol.dose_amount, 3)
         self.assertEqual(protocol.interval, 4)
@@ -20,24 +19,24 @@ class Dosing_Protocol_test(unittest.TestCase):
         
         
         #Test Dosing_Protocol with optional parameters unspecified
-        protocol = pk.Dosing_Protocol(1)
+        protocol = Dosing_Protocol(1)
         self.assertEqual(protocol.dose_amount, 0)
         self.assertEqual(protocol.interval, 0)
         self.assertEqual(protocol.duration, 0)
         
         #Test for expected exceptions now
         with self.assertRaises(ValueError):
-            protocol = pk.Dosing_protocol(2, -3, 4, 5) # A negative dose amount should raise a value error.
+            protocol = Dosing_protocol(2, -3, 4, 5) # A negative dose amount should raise a value error.
         with self.assertRaises(ValueError):
-            protocol = pk.Dosing_protocol(2, 3, -4, 5) # A negative interval time should raise a value error.
+            protocol = Dosing_protocol(2, 3, -4, 5) # A negative interval time should raise a value error.
         with self.assertRaises(ValueError):
-            protocol = pk.Dosing_protocol(2, 3, 4, -5) # A negative duration time for drug administration should raise a value error.
+            protocol = Dosing_protocol(2, 3, 4, -5) # A negative duration time for drug administration should raise a value error.
         with self.assertRaises(TypeError):
-            protocol = pk.Dosing_protocol(1, 2, 3) # A non-zero interval time for dosing method 1 should raise a type error.
+            protocol = Dosing_protocol(1, 2, 3) # A non-zero interval time for dosing method 1 should raise a type error.
         with self.assertRaises(TypeError):
-            protocol = pk.Dosing_protocol(1, 2, 0, 4) # A nonzero duration time for drug administration for dosing method 1 should raise a type error.
+            protocol = Dosing_protocol(1, 2, 0, 4) # A nonzero duration time for drug administration for dosing method 1 should raise a type error.
         with self.assertRaises(ValueError):
-            protocol = pk.Dosing_protocol(3, 4, 5, 6) # Any value other than 1 or 2 for the dosing method should raise a value error.
+            protocol = Dosing_protocol(3, 4, 5, 6) # Any value other than 1 or 2 for the dosing method should raise a value error.
         
         
         
@@ -46,7 +45,7 @@ class Dosing_Protocol_test(unittest.TestCase):
         Tests the get_dose method for different dosing methods.
         """
         # Test Dosing_protocol with dosing_method 1
-        protocol = pk.Dosing_Protocol(1, 10)  # Single, time-independent administration
+        protocol = Dosing_Protocol(1, 10)  # Single, time-independent administration
         self.assertEqual(protocol.get_dose(0), 10)
         self.assertEqual(protocol.get_dose(5), 0)  # No dosing after t = 0
 
@@ -69,20 +68,20 @@ class Dosing_Protocol_test(unittest.TestCase):
        Tests the get_dose_function method for different dosing methods.
        """
        # Test Dosing_protocol with dosing_method 1
-       protocol = pk.Dosing_Protocol(1, 10)  # Single, time-independent administration
+       protocol = Dosing_Protocol(1, 10)  # Single, time-independent administration
        dose_function = protocol.get_dose_function()
        self.assertEqual(dose_function(0), 10)
        self.assertEqual(dose_function(5), 0)
 
        # Test Dosing_protocol with dosing_method 2
-       protocol = pk.Dosing_Protocol(2, 7, 3, 10)  # Repeated administration at intervals
+       protocol = Dosing_Protocol(2, 7, 3, 10)  # Repeated administration at intervals
        dose_function = protocol.get_dose_function()
        self.assertEqual(dose_function(0), 7)
        self.assertEqual(dose_function(5), 0)
        self.assertEqual(dose_function(12), 0)
 
        # Test for invalid dosing_method
-       protocol = pk.Dosing_Protocol(3)
+       protocol = Dosing_Protocol(3)
        with self.assertRaises(ValueError):
            dose_function = protocol.get_dose_function()
 
